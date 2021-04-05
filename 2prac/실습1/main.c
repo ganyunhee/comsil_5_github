d789ee25356be7db8d9adccd3cd5739b27a7bee4
unknown <eunicenellgan@gmail.com>
2021-04-05 15:31:22 +0900

    실습1

---
+++ 2prac/실습1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main() {
+	printf("\n");
+	printf("I'm a compiled executable program of a Dog, a Black Cow, and a Turtle :)\n");
+	printf("Type 'make clean' and all the animals' object files will disappear.\n");
+	printf("\n");
+}
